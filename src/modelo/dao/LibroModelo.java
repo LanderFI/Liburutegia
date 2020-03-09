32bc3d67229abad4c1aabcdd6324a2602722e7e5
lander <lander@DESKTOP-18MM8IU>
2020-03-09 09:26:33 +0100

    Liburutegia

---
+++ src/modelo/dao/LibroModelo.java
@@ -0,0 +1,16 @@
+package modelo.dao;
+
+import java.util.ArrayList;
+
+import modelo.Conector;
+import modelo.bean.Libro;
+
+public class LibroModelo extends Conector{
+	
+	public ArrayList<Libro> select(int id) {
+		
+		return null;
+	}
+	
+	
+}
