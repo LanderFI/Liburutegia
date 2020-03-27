b7d1cc40d68fd3a7dce4b8d5674a6924cebeade5
Lander <Lander@LanderFI>
2020-03-27 12:24:02 +0100

    Prestamo java entregado eta instrito metodoak eginda

--- src/modelo/bean/Prestamo.java
+++ src/modelo/bean/Prestamo.java
@@ -1,12 +1,15 @@
 package modelo.bean;
 
 import java.sql.Date;
+import java.util.ArrayList;
 
 public class Prestamo {
 	private Libro libro;
 	private Socio socio;
 	private boolean devuelto;
 	private Date fecha;
+	private boolean entregado;
+	private boolean inscrito;
 	
 	public Libro getLibro() {
 		return libro;
@@ -32,7 +35,29 @@ public class Prestamo {
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
-
 	
+	public void setEntregado(boolean entregado) {
+		this.entregado = entregado;
+	}
+	public boolean esEntregado() {
+		// TODO Auto-generated method stub
+		return entregado;
+	}
+	public void setInscrito(boolean inscrito) {
+		this.inscrito = inscrito;
+	}
+	
+	public boolean esInscrito() {
+		
+		return inscrito;
+	}
+	public ArrayList<Prestamo> selectAll(){
+		
+		ArrayList<Libro> libros = new ArrayList<Libro>();
+		ArrayList<Socio> socios = new ArrayList<Socio>();
+		
+		return null;
+		
+	}
 	
 }
